f0caf741d1e2f2c1901ee7e70f6dd3abc697f5f3
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-02-17 21:01:44 +0600

    all calculation complite

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,88 @@
-function calculate() {
-// ------------------Total Income-------------------------//
-    const income = document.getElementById('income').value ;
-    const totalIncome = parseFloat(income);
-    
-//--------------------Total Expenses------------------------//
-    const foodCosts = document.getElementById('food-costs').value ;
-    const rentCosts = document.getElementById('rent-costs').value ;
-    const clothesCosts = document.getElementById('clothes-costs').value ;
-    
-//----------------- add total expenses-----------------------//
-     const totalExpenses = parseFloat(foodCosts) + parseFloat(rentCosts) + parseFloat(clothesCosts);
 
-//-------------Balance and expenses calculation-------------//
-    document.getElementById('total-expenses').innerText = totalExpenses ;
-    const totalBalance = totalIncome - totalExpenses ;
-    document.getElementById('total-balance').innerText = totalBalance ;
+// ID SELECTING
+const incomeField = document.getElementById("income-field");
+const foodField = document.getElementById("food-field");
+const rentField = document.getElementById("rent-field");
+const clothesField = document.getElementById("clothes-field");
+const calculateBtn = document.getElementById("calculate-button");
+const balanceId = document.getElementById("balanceId");
+
+const totalExpenses = document.getElementById("total-expenses");
+const saveField = document.getElementById("save_field");
+const saveBtn = document.getElementById("save-btn-hero");
+const savingAmount = document.getElementById("saving-amount");
+const totalRemainingBalance = document.getElementById("remaining-balance");
+
+// TOTAL EXPENSES
+calculateBtn.addEventListener("click", function () {
+  isEmptyError();
+  handleError();
+  addition();
+  totalBalance();
+});
+
+// Addition Function
+function addition() {
+  const foodAmount = parseInt(foodField.value);
+  const rentAmount = parseInt(rentField.value);
+  const clothesAmount = parseInt(clothesField.value);
+
+  const TotalExpense = foodAmount + rentAmount + clothesAmount;
+  totalExpenses.innerText = TotalExpense.toFixed(0);
 }
 
-//------------------- percentage calculations ----------------------//
-function saveButton(){
-    const income = document.getElementById('income').value ;
-    const totalIncome = parseFloat(income);
+// BALANCE
+function totalBalance() {
+  const income = parseInt(incomeField.value);
+  const totalExpensesValue = parseInt(totalExpenses.innerText);
+  const balance = income - totalExpensesValue;
+  balanceId.innerText = balance.toFixed(0);
+}
 
-    const saveInput = document.getElementById ('save-input').value ;
-    const saveAmount = parseFloat (saveInput);
+// Saving Function Click
+saveBtn.addEventListener("click", function () {
+  handleError();
+  savePrice();
+});
 
-    // percentage
-    saveTotalAmount = (totalIncome * saveAmount)/100 ;
-    document.getElementById('saving-amount').innerText = saveTotalAmount ;
+// SAVING
+function savePrice() {
+  var incomeFieldValue = parseInt(incomeField.value);
+  var saveFieldValue = parseInt(saveField.value);
 
-    // calculate remaining Balance
-    const balance = document.getElementById('total-balance').innerText;
-    const totalBalance = parseFloat(balance);
+  var totalValue = (incomeFieldValue * saveFieldValue)/100;
+  savingAmount.innerText = totalValue.toFixed(0);
 
-    remainingBalance = totalBalance - saveTotalAmount ;
-    document.getElementById('remaining-balance').innerText = remainingBalance ;
+  totalRemainingBalance.innerText =
+    balanceId.innerText - savingAmount.innerText;
+}
+
+/* ERROR HANDLING */
+function isEmptyError() {
+  if (incomeField.value === "") {
+    alert("Field Must be not Empty");
+    return;
+  }
+}
+
+function handleError() {
+  if (
+    (incomeField.value ||
+      foodField.value ||
+      rentField.value ||
+      clothesField.value ||
+      saveField.value) < 0
+  ) {
+    alert("Please Insert only positive Number");
+    return;
+  } else if (
+    isNaN(incomeField.value) ||
+    isNaN(foodField.value) ||
+    isNaN(rentField.value) ||
+    isNaN(clothesField.value) ||
+    isNaN(saveField.value) === true
+  ) {
+    alert("Please insert a valid number");
+    return;
+  }
 }
